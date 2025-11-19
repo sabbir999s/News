4b58ca737a6e0099bab660cf4a424b60a194a409
sabbir999s <98657435+sabbir999s@users.noreply.github.com>
2025-11-19 22:46:55 +0600

    Update news.js

--- news.js
+++ news.js
@@ -1,6 +1,6 @@
 // আপনার AdsGram ব্লক আইডি এখানে বসান
-const VIDEO_BLOCK_ID = "your-block-id-video"; 
-const INTERSTITIAL_BLOCK_ID = "your-block-id-interstitial";
+const VIDEO_BLOCK_ID = "17904"; 
+const INTERSTITIAL_BLOCK_ID = "int-17904";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBQf-ILFW6jDV-c_O2LG6elA5oAB84p2XQ",
